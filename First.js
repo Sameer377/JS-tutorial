3b01dfcef58f831670a6a2d228dfb76dc49b40c5
Sameer Shaikh <sameershaikh01072004@gmail.com>
2023-05-22 06:16:58 +0530

    All Basics of JS

--- First.js
+++ First.js
@@ -17,3 +17,37 @@ for(i=0;i<arr.length;i++){
 arr.forEach(function(ele){
     console.log(ele);
 })
+console.clear()
+arr =[1,2,"sameer",4,5]
+//remove last element
+console.log(arr ,'length : ' +arr.length);
+arr.pop();
+//append ele at the end of arr
+arr.push("sameer");
+//delete first ele
+arr.shift();
+console.log("length : ",arr.length);
+arr=[2,3,1,4]
+arr.sort();
+console.log(arr);
+
+
+//strings
+
+let str="hello sameer";
+
+console.log(str);
+
+str.replace("hello","hi");
+console.log(str);
+
+let j = str.indexOf("e");
+console.log(j);
+j=str.lastIndexOf("e");
+console.log(j); 
+
+console.log(str.slice(1,3));
+console.log(str);
+
+let mydate=new Date();
+console.log(mydate);
